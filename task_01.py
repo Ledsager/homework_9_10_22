23dac6ab4fee842f4e4dac91605edbc2ecfe1825
«Rustam-Ledsager» <r.yakupov81@yandex.ru>
2022-10-11 00:38:00 +0300

    added task_01(correct)+task_02

--- task_01.py
+++ task_01.py
@@ -8,11 +8,12 @@
 
 def DigitsSum(elem):
     result = 0
-    i=0
+    i = 0
     for i in elem:
-        if (i!='.') and (i!='-'):
+        if (i != '.') and (i != '-'):
             result = result + int(i)
     return result
+
 
 print('Введите вещественное число:')
 n = float(input())
