861225f3b54a6460fd591fdcdfb24ec634c25901
«Rustam-Ledsager» <r.yakupov81@yandex.ru>
2022-10-10 23:51:18 +0300

    added task_01

---
+++ task_01.py
@@ -0,0 +1,20 @@
+# Напишите программу, которая принимает на вход вещественное
+# число и показывает сумму его цифр.
+
+# Пример:
+
+# - 6782 -> 23
+# - 0,56 -> 11
+
+def DigitsSum(elem):
+    result = 0
+    i=0
+    for i in elem:
+        if (i!='.') and (i!='-'):
+            result = result + int(i)
+    return result
+
+print('Введите вещественное число:')
+n = float(input())
+elem = str(n)
+print(DigitsSum(elem))
