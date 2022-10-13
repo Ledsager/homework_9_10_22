74d2f9c2877359032c7d6f4446e07ee2200b358a
«Rustam-Ledsager» <r.yakupov81@yandex.ru>
2022-10-14 00:58:03 +0300

    added task_05 correct

--- task_05.py
+++ task_05.py
@@ -6,36 +6,43 @@ from random import randint
 
 def digits_mas_random(list_a):
     mas = []
-    k = len(list_a)-1
-    mas.append(randint(0, k))
+    k = len(list_a)
+    mas.append(randint(1, k))
     i = 0
-    while i < k:
-        j = randint(0, k)
+    while i < k-1:
+        j = randint(1, k)
         if not (j in mas):
             mas.append(j)
             i = i+1
-    print(mas)
+    # print(mas)
     return mas
+
+# ввод списка через пробел
 
 
 def input_mas():
     mas = []
-    mas = [int(i) for i in input().split()]
+    mas = [i for i in input().split()]
     return mas
 
+# используя список случайных позиций (list_random_index) формирую доп список в которой записываю значения из заданого
+# списка позиции,индексы которых совпадают с сформированным списком случайных позиций
 
-# print('Введите массив значений : ')
-# list_a = input_mas()
-list_a = [1, 23, 'rt', 'u', 234, 6, 'dr4', 214]
+
+def random_list_a(list_a):
+    list_temp = []
+    for i in range(0, len(list_a)):
+        temp_b = int(list_random_index[i])-1
+        # print(list_a[temp_b],'-',list_random_index[i])
+        list_temp.append(list_a[temp_b])
+    list_a=list_temp
+    return list_a
+
+
+print('Введите массив переменных : ')
+list_a = input_mas()
+# list_a = [1, 23, 'rt', 'u', 234, 6, 'dr4', 214,5]
 list_random_index = digits_mas_random(list_a)
-
-for i in range(0,4):
-    # print(list_a[i],' - ', list_random_index[i])
-    temp_b=int(list_random_index[i])
-    print(temp_b,' - ',list_a[temp_b])
-    temp_a = list_a[i]
-    list_a[i] = list_a[temp_b]
-    list_a[temp_b] = temp_a
-
 print(list_random_index)
 print(list_a)
+print(random_list_a(list_a))
